55e26e3760f728ed63dc777021043ed6ab9ac28b
Ajaykumar Nadar <ajaykumarn3000@gmail.com>
2024-07-28 14:28:17 +0530

    page loading

--- asserts/js/index.js
+++ asserts/js/index.js
@@ -1,3 +1,11 @@
 const d = new Date();
 let year = d.getFullYear();
-$('#year').text(year);
+$("#year").text(year);
+
+window.addEventListener("load", () => {
+  const main = document.getElementById("main");
+  const loader = document.getElementById("loading");
+  main.classList.toggle("inactive");
+  loader.classList.toggle("inactive");
+  loader.remove();
+});
